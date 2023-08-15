0c2ad09281dd376832b773f6595e19c092f7273f
longGCO <ngoclong3066@gmail.com>
2023-08-15 09:53:00 +0700

    test html

--- js/main.js
+++ js/main.js
@@ -14,11 +14,15 @@ function openTab(event, tabName) {
 }
 
 // Open the first tab by default
-document.getElementById("tab-1").style.display = "block";
-document.querySelector(".content_tablinks").classList.add("active");
+// document.getElementById("tab-1").style.display = "block";
+// document.querySelector(".content_tablinks").classList.add("active");
 // mobile menu
-var mobileMenu = document.querySelector("#mobile-menu");
-mobileMenu.onclick = function () {
-  let navbar = document.querySelector(".navbar-menu");
-  navbar.classList.toggle("active");
-};
+const mobileMenu = document.querySelector("#mobile-menu");
+// mobileMenu.onclick = function () {
+//   let navbar = document.querySelector(".navbar-menu");
+//   navbar.classList.toggle("active");
+// };
+const navbar_menu = document.querySelector(".navbar-menu");
+mobileMenu.addEventListener("click", () => {
+  navbar_menu.classList.toggle("active");
+});
